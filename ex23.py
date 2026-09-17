36a0f20e62a942d1860bc41ec8047252cde6e637
Yasmin lopes <lopesyasmin@hotmail.com>
2026-09-17 15:00:27 +0000

    Add files via upload
    Receba 3 valores obrigatoriamente em ordem crescente e um 4o valor não necessariamente
    em ordem. Mostre os 4 números em ordem crescente.

---
+++ ex23.py
@@ -0,0 +1,16 @@
+#Receba 3 valores obrigatoriamente em ordem crescente e um 4o valor não necessariamente em ordem. Mostre os 4 números em ordem crescente.
+print('---- Valores em ordem crescente ----')
+n1 = int(input('Primeiro valor: '))  
+n2 = int(input('Segundo valor: ')) 
+n3 = int(input('Terceiro valor: ')) 
+print('---- Valor não necessariamente em ordem ----')
+n4 = int(input('Quarto valor: ')) 
+print('')
+if n4 <= n1: 
+    print('Ordem crescente: ',n4, n1, n2, n3)
+elif n4 >= n1 and n4 <= n2:
+    print('Ordem crescente: ',n1, n4, n2, n3)
+elif n4 >= n2 and n4 <= n3:
+    print('Ordem crescente: ',n1, n2, n4, n3)
+else:
+    print('Ordem crescente: ',n1, n2, n3, n4)
